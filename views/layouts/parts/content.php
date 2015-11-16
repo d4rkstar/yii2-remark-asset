85e744efc32ae80d451e4f0338e7ae339bf25f34
d4rkstar <d4rkstar@gmail.com>
2015-11-16 23:44:50 +0100

    moved files to parts

---
+++ views/layouts/parts/content.php
@@ -0,0 +1,28 @@
+<!-- Page -->
+<?php
+use yii\widgets\Breadcrumbs;
+?>
+<div class="page animsition">
+    <div class="page-header">
+        <h1 class="page-title"><?php
+            if ($this->title !== null) {
+                echo \yii\helpers\Html::encode($this->title);
+            } else {
+                echo \yii\helpers\Inflector::camel2words(
+                    \yii\helpers\Inflector::id2camel($this->context->module->id)
+                );
+                echo ($this->context->module->id !== \Yii::$app->id) ? '<small>Module</small>' : '';
+            } ?></h1>
+        <?php
+        print Breadcrumbs::widget(
+            [
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]
+        )
+        ?>
+    </div>
+    <div class="page-content container-fluid">
+        <?= $content; ?>
+    </div>
+</div>
+<!-- End Page -->
